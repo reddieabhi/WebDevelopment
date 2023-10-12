e806f585892b5d53e0330d04b393368bc642ad38
reddieabhi <91117253+reddieabhi@users.noreply.github.com>
2023-10-12 10:54:14 +0530

    add some more  changes to vercel

--- WebDevelopment/settings.py
+++ WebDevelopment/settings.py
@@ -140,8 +140,8 @@ MEDIA_URL = "/media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATICFILES_DIR = os.path.join(BASE_DIR,"static"),
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build","static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles_build")
 
 
 
